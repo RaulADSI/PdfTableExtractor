92282330ea6781d1345b5ceedec50b7faaaddadb
RaulADSI <rauramirez25@gmail.com>
2025-02-28 14:16:21 -0500

    Creacion del Jar y clase main que contiene otras main

---
+++ src/main/java/rulodev/pdfreader/Main.java
@@ -0,0 +1,16 @@
+package rulodev.pdfreader;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Ejecutar el metodo main de PDFTableExtractor2
+        System.out.println("Ejecutando PDFTableExtractor2");
+        PDFTableExtractor2.main(args);
+
+        //Ejecutar el metodo main de CSVKeywordSearch
+        System.out.println("Ejecutando CSVKeywordSearch");
+        CSVKeywordSearch.main(args);
+    }
+
+}
